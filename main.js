c75d5dd85a58e1ece62e9ccaa1aa2ecc930c7f5c
Dmitriy Garbuz <51447305+DmitriyGarbuz@users.noreply.github.com>
2019-08-01 22:04:44 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -42,75 +42,164 @@ buttonJ.addEventListener('mouseout',changeColorReset);
 buttonK.addEventListener('mouseover', changeColor);
 buttonK.addEventListener('mouseout',changeColorReset);
 
-// let audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3');
-// audio.play();
+
 
 
     buttonA.addEventListener("click", audioA );
     function audioA(){
-        musikA.play(),
-        this.style.background = '#e4fd00c9';
-        this.style.transition = '1s';
+        musikA.play();
+        const a = buttonA.style;
+        function changeColor(){
+            a.background = '#e4fd00c9';
+            a.transition = '.5s';
+        }
+        changeColor();
+        function changeColorReset(){
+            a.background = 'white';
+        }
+        setTimeout(changeColorReset, 500);
     }
     buttonS.addEventListener("click", audioS );
     function audioS(){
-        musikS.play(),
-        this.style.background = '#e4fd00c9';
-        this.style.transition = '1s';
+        musikS.play();
+       const s = buttonS.style;
+        function changeColor(){
+            s.background = '#e4fd00c9';
+            s.transition = '.5s';
+        }
+        changeColor();
+        function changeColorReset(){
+            s.background = 'white';
+        }
+        setTimeout(changeColorReset, 500);
     }
     buttonD.addEventListener("click", audioD );
     function audioD(){
-        musikD.play(),
-        this.style.background = '#e4fd00c9';
-        this.style.transition = '1s';
+        musikD.play();
+        const d = buttonD.style;
+        function changeColor(){
+            d.background = '#e4fd00c9';
+            d.transition = '.5s';
+        }
+        changeColor();
+        function changeColorReset(){
+            d.background = 'white';
+        }
+        setTimeout(changeColorReset, 500);
     }
     buttonF.addEventListener("click", audioF );
     function audioF(){
-        musikF.play(),
-        this.style.background = '#e4fd00c9';
-        this.style.transition = '1s';
+        musikF.play();
+        const f = buttonF.style;
+        function changeColor(){
+            f.background = '#e4fd00c9';
+            f.transition = '.5s';
+        }
+        changeColor();
+        function changeColorReset(){
+            f.background = 'white';
+        }
+        setTimeout(changeColorReset, 500);
     }
     buttonG.addEventListener("click", audioG );
     function audioG(){
-        musikG.play(),
-        this.style.background = '#e4fd00c9';
-        this.style.transition = '1s';
+        musikG.play();
+        const g = buttonG.style;
+        function changeColor(){
+            g.background = '#e4fd00c9';
+            g.transition = '.5s';
+        }
+        changeColor();
+        function changeColorReset(){
+            g.background = 'white';
+        }
+        setTimeout(changeColorReset, 500);
     }
     buttonH.addEventListener("click", audioH );
     function audioH(){
-        musikH.play(),
-        this.style.background = '#e4fd00c9';
-        this.style.transition = '1s';
+        musikH.play();
+        const h = buttonH.style;
+        function changeColor(){
+            h.background = '#e4fd00c9';
+            h.transition = '.5s';
+        }
+        changeColor();
+        function changeColorReset(){
+            h.background = 'white';
+        }
+        setTimeout(changeColorReset, 500);
     }
     buttonJ.addEventListener("click", audioJ );
     function audioJ(){
-        musikJ.play(),
-        this.style.background = '#e4fd00c9';
-        this.style.transition = '1s';
+        musikJ.play();
+        const j = buttonJ.style;
+        function changeColor(){
+            j.background = '#e4fd00c9';
+            j.transition = '.5s';
+        }
+        changeColor();
+        function changeColorReset(){
+            j.background = 'white';
+        }
+        setTimeout(changeColorReset, 500);
     }
     buttonK.addEventListener("click", audioK );
     function audioK(){
-        musikK.play(),
-        this.style.background = '#e4fd00c9';
-        this.style.transition = '1s';
+        musikK.play();
+        const k = buttonK.style;
+        function changeColor(){
+            k.background = '#e4fd00c9';
+            k.transition = '.5s';
+        }
+        changeColor();
+        function changeColorReset(){
+            k.background = 'white';
+        }
+        setTimeout(changeColorReset, 500);
     }
 
     window.addEventListener('keypress', function(event){
         switch(event.keyCode) {
+            case 65: audioA()
+            case 1060: audioA()
             case 97: audioA()
+            case 1092: audioA()
                 break;
+            case 83: audioS()
+            case 1067: audioS()
+            case 1099: audioS() 
+            case 1110: audioS() 
+            case 1030: audioS()
             case 115: audioS()
                 break;
+            case 1042: audioD()
+            case 1074: audioD()
+            case 68: audioD()
             case 100: audioD()
                 break;
+            case 1072: audioF()
+            case 1040: audioF()
+            case 70: audioF()
             case 102: audioF()
                 break;
+            case 1087: audioG()
+            case 1055: audioG()
+            case 71: audioG()
             case 103: audioG()
                 break;
+            case 1088: audioH()
+            case 1056: audioH()
+            case 72: audioH()
             case 104: audioH()
                 break;
+            case 1086: audioJ()
+            case 1054: audioJ()
+            case 74: audioJ()
             case 106: audioJ()
                 break;
+            case 1083: audioK()
+            case 1051: audioK()
+            case 75: audioK()
             case 107: audioK()
                 break;
         }
